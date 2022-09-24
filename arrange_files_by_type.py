1e852d39b6bbcffff464c2a1569dfbd327e82d59
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2022-09-24 21:34:10 +0100

    Added Main files
    These files sort the contents of the directory selected and sorts the contents based on type of file

---
+++ arrange_files_by_type.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+
+
+def sorter():
+    try:
+        user_source = input('Enter your PC name: ')
+        source = input('Enter the source directory. e.g Downloads: ')
+        path = 'C:/Users/%s/%s'%(user_source, source)
+
+        # path = 'C:/Users/USER/Downloads'
+        # destination = 'C:/Users/USER/Desktop/'
+
+        destination = 'C:/Users/%s'%user_source
+
+
+        list_ = os.listdir(path)
+
+        for file_ in list_:
+            name, ext = os.path.splitext(file_)
+
+            ext = ext[1:]
+            src_path = os.path.join(path, file_)
+            
+                #Videos
+            if ext in ['mp4', 'mkv', '3gp', 'mov', 'avi', 'webm', 'flv', 'wmv'] and os.path.exists(destination+'/'+'Videos'):
+                print('Moving Video files to %s/Videos'%destination)
+                shutil.move(src_path,destination+'/'+'Videos'+ '/'+file_)
+                
+            elif ext in ['mp4', 'mkv', '3gp', 'mov', 'avi', 'webm', 'flv', 'wmv']:
+                os.makedirs(destination+'/'+'Videos')
+                shutil.move(src_path, destination+'/'+'Videos'+ '/'+file_)
+
+                #Pictures
+            elif ext in ['png', 'jpg', 'bmp', 'jpeg', 'gif','ico', 'jpe', 'webp'] and os.path.exists(destination+'/'+'Pictures'):
+                print('Moving Image files to %s/Pictures'%destination)
+                shutil.move(src_path, destination+'/'+ 'Pictures'+'/'+file_)
+            
+            elif ext in ['png', 'jpg', 'bmp', 'jpeg', 'gif','ico', 'jpe', 'webp']:    
+                os.makedirs(destination+'/'+'Pictures')
+                shutil.move(src_path, destination+'/'+ 'Pictures'+'/'+file_)
+                
+                #Documents
+            elif ext in ['pdf', 'doc', 'docx', 'txt', 'rtf','epub', 'mobi', 'csv', 'ipynb'] and os.path.exists(destination+'/'+'Documents'):
+                print('Moving Document files to %s/Documents'%destination)
+                shutil.move(src_path, destination+'/'+ 'Documents'+'/'+file_)
+            
+            elif ext in ['pdf', 'doc', 'docx', 'txt', 'rtf','epub', 'mobi', 'csv', 'ipynb']:      
+                os.makedirs(destination+'/'+'Documents')
+                shutil.move(src_path, destination+'/'+ 'Documents'+'/'+file_)
+
+               #Subtitles
+            elif ext in ['srt'] and os.path.exists(destination+'/'+'Subtitles'):
+                print('Moving Subtitle files to %s/Subtitles'%destination)
+                shutil.move(src_path, destination+'/'+ 'Subtitles'+'/'+file_)
+            
+            elif ext in ['srt']:      
+                os.makedirs(destination+'/'+'Subtitles')
+                shutil.move(src_path, destination+'/'+ 'Subtitles'+'/'+file_)
+
+                #ZipArchives
+            elif ext in ['zip', 'rar', '7z', 'gz'] and os.path.exists(destination+'/'+'Archive'):
+                print('Moving Zip files to %s/Archive'%destination)
+                shutil.move(src_path, destination+'/'+ 'Archive'+'/'+file_)
+            
+            elif ext in ['zip', 'rar', '7z', 'gz']:      
+                os.makedirs(destination+'/'+'Archive')
+                shutil.move(src_path, destination+'/'+ 'Archive'+'/'+file_)
+
+            else:
+                ('No file to Move!!!')
+    except KeyboardInterrupt:
+        exit('\nClosing!!!')
+    except FileNotFoundError:
+        print('Invalid Directory!!')
+        return sorter()
+
+sorter()
---
+++ sort_a_folder.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+
+user_source = input('Enter your PC name: ')
+source = input('Enter the source directory. e.g Downloads: ').capitalize()
+path = 'C:/Users/%s/%s'%(user_source, source)
+
+list_ = os.listdir(path)
+
+for file_ in list_:
+    name, ext = os.path.splitext(file_)
+
+
+    ext = ext[1:]
+        #Videos
+    if ext in ['mp4', 'mkv'] and os.path.exists(path+'/'+'Vids'):
+        print('Moving Videos to %s/Vids'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Vids'+ '/'+file_)
+        
+    elif ext in ['mp4', 'mkv'] and os.path.exists(path+'/'+'Vids') == False:
+        os.makedirs(path+'/'+'Vids')
+        shutil.move(path+'/'+file_, path+'/'+'Vids'+ '/'+file_)
+
+        #Pictures
+    elif ext in ['png', 'jpg', 'bmp', 'jpeg', 'gif','ico', 'jpe', 'webp'] and os.path.exists(path+'/'+'Pictures'):
+        print('Moving Pictures to %s/Pictures'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Pictures'+'/'+file_)
+    
+    elif ext in ['png', 'jpg', 'bmp', 'jpeg', 'gif','ico', 'jpe', 'webp'] and os.path.exists(path+'/'+'Pictures') == False:      
+        os.makedirs(path+'/'+'Pictures')
+        shutil.move(path+'/'+file_, path+'/'+'Pictures'+ '/'+ file_)
+        
+        #Documents
+    elif ext in ['pdf', 'doc', 'docx', 'txt', 'rtf','epub', 'mobi', 'csv', 'ipynb'] and os.path.exists(path+'/'+'Documents'):
+        print('Moving Documentsto %s/Documents'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Documents'+'/'+file_)
+    
+    elif ext in ['pdf', 'doc', 'docx', 'txt', 'rtf','epub', 'mobi', 'csv', 'ipynb'] and os.path.exists(path+'/'+'Documents') == False:      
+        os.makedirs(path+'/'+'Documents')
+        shutil.move(path+'/'+file_, path+'/'+'Documents'+ '/'+ file_)
+       
+       
+       #Subtitles
+    elif ext in ['srt'] and os.path.exists(path+'/'+'Subtitles'):
+        print('Moving Subtitles to %s/Subtitles'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Subtitles'+'/'+file_)
+    
+    elif ext in ['srt'] and os.path.exists(path+'/'+'Subtitles') == False:      
+         os.makedirs(path+'/'+'Subtitles')
+         shutil.move(path+'/'+file_, path+'/'+'Subtitles'+ '/'+ file_)
+    
+        #ZipArchives
+    elif ext in ['zip', 'rar', '7z', 'gz'] and os.path.exists(path+'/'+'Archive'):
+        print('Moving Zips to %s/Archive'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Archive'+'/'+file_)
+    
+    elif ext in ['zip', 'rar', '7z', 'gz'] and os.path.exists(path+'/'+'Archive') == False:      
+        os.makedirs(path+'/'+'Archive')
+        shutil.move(path+'/'+file_, path+'/'+'Archive'+ '/'+ file_)
+       
+    elif ext in ['html', 'htm'] and os.path.exists(path+'/'+'Html'):
+        print('Moving Html to %s/Html'%path)
+        shutil.move(path+'/'+file_, path+'/'+'Html'+'/'+file_)
+    
+    elif os.path.exists(path+'/'+'Html') == False:      
+        os.makedirs(path+'/'+'Html')
+        shutil.move(path+'/'+file_, path+'/'+'Html'+ '/'+ file_)
+
